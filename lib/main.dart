afae72764ad95050320b8aafad02b370a4201e1a
S-Khavin <khavin2004@gmail.com>
2024-04-29 12:50:32 +0530

    Added player

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flame/game.dart';
 import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon24/player.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Supabase.initialize(
+    url: 'https://srneluayrtckhtrwdfqd.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNybmVsdWF5cnRja2h0cndkZnFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyMTA5NjAsImV4cCI6MjAyOTc4Njk2MH0.kH-dnm69SGX8gS5wlBYKdonZkY2KzfK59g_cog2idE4',
+  );
   RiveFile riveFile = await RiveFile.asset('assets/rive/rive.riv');
   Flame.device.fullScreen();
   runApp(GameWidget(game: SumoWrestlerGame(riveFile: riveFile)));
@@ -15,14 +20,18 @@ void main() async {
 class SumoWrestlerGame extends FlameGame with HasGameRef {
   final RiveFile riveFile;
   SumoWrestlerGame({required this.riveFile});
-  
+  late Player player;
+  late Player opponent;
   @override
   Future<void> onLoad() async {
-    add(
-      Player(playerArtboard: riveFile.artboardByName('Player')!)
-        ..position = Vector2(gameRef.size.x / 2, gameRef.size.y / 2)
-        ..anchor = Anchor.center
-        ..size = Vector2(25, 25),
-    );
+    player =
+        Player(playerArtboard: riveFile.artboardByName('Player')!, isMe: true)
+          ..position = Vector2(gameRef.size.x / 2, gameRef.size.y / 2)
+          ..anchor = Anchor.center
+          ..size = Vector2(25, 25);
+    opponent =
+        Player(playerArtboard: riveFile.artboardByName('Player')!, isMe: false)
+          ..position = Vector2(gameRef.size.x / 2, gameRef.size.y / 2)
+          ..size = Vector2(25, 25);
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,13 +1,15 @@
 import 'package:flame/components.dart';
 import 'package:flame_rive/flame_rive.dart';
 
-class Player extends RiveComponent with HasGameRef{
+class Player extends RiveComponent with HasGameRef {
   final Artboard playerArtboard;
   late final RiveComponent player;
-  Player({required this.playerArtboard}): super(artboard: playerArtboard,
-  );
-  
-  Future<void> onLoad() async{
+  Player({required this.playerArtboard, required bool isMe})
+      : _isMyPlayer = isMe,
+        super(artboard: playerArtboard);
+  final bool _isMyPlayer;
+
+  Future<void> onLoad() async {
     player = RiveComponent(artboard: playerArtboard);
   }
 }
